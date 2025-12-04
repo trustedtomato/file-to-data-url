82f8e2ce4ab6aeac4c62149c528b8a6e9ed9fb17
Tamas Halasi <trusted.tomato@gmail.com>
2025-12-04 15:31:47 +0100

    add deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
+  base: '/file-to-data-url/',
+  plugins: [svelte()]
 })
